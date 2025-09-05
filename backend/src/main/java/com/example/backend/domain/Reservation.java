b74edafddcdfeb7f0cdaa7cd171ec7e2f200653a
JHS5020 <goals00002@naver.com>
2025-09-05 21:48:31 +0900

    09_05
    예약 정보, 객실 엔티티

--- backend/src/main/java/com/example/backend/domain/Reservation.java
+++ backend/src/main/java/com/example/backend/domain/Reservation.java
@@ -1,4 +1,42 @@
 package com.example.backend.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Reservation")
 public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Reservation_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private Date checkInDate;
+
+    @Column(nullable = false)
+    private Date checkOutDate;
+
+    @Column()
+    private BigDecimal discount;
+
+    @Column()
+    private BigDecimal taxes;
+
+    @Column(nullable = false)
+    private BigDecimal totalPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id", nullable = false)  // roomId 컬럼과 매핑
+    private Room room;
 }
--- backend/src/main/java/com/example/backend/domain/Room.java
+++ backend/src/main/java/com/example/backend/domain/Room.java
@@ -1,4 +1,40 @@
 package com.example.backend.domain;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "room")
 public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Room_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(length = 20, nullable = false)
+    private String name;
+
+    @Column(length = 20)
+    private String view;
+
+    @Column(length = 50)
+    private String bed;
+
+    @Column(length = 50, nullable = false)
+    private String roomsGuest;
+
+    @ManyToOne
+    @JoinColumn(name = "hotel_id", nullable = false)
+    private Hotel hotel;
 }
