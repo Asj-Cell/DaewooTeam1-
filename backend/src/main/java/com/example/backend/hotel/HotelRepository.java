01cdad3bce7347c3c1a3886db8af413333df2a0a
JHS5020 <goals00002@naver.com>
2025-09-12 21:06:53 +0900

    필요없는거 정리

--- backend/src/main/java/com/example/backend/hotel/HotelRepository.java
+++ backend/src/main/java/com/example/backend/hotel/HotelRepository.java
@@ -7,7 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface HotelRepository extends JpaRepository<Hotel, Long>, JpaSpecificationExecutor<Hotel> {
-
-
-
 }
