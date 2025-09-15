e9b057840dad98f57301d9ca8e33b4d2428879e6
JHS5020 <goals00002@naver.com>
2025-09-15 20:12:53 +0900

    찜 엔티티

---
+++ backend/src/main/java/com/example/backend/favorites/repository/FavoritesRepository.java
@@ -0,0 +1,9 @@
+package com.example.backend.favorites.repository;
+
+import com.example.backend.hotel.entity.Favorites;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FavoritesRepository extends JpaRepository<Favorites, Long> {
+}
