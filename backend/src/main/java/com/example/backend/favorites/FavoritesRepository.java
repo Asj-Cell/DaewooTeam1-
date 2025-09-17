2e823587b2c6458af8d2e371d7c5b39ecdddf70f
JHS5020 <goals00002@naver.com>
2025-09-17 18:53:26 +0900

    서비스 만듦

--- backend/src/main/java/com/example/backend/favorites/FavoritesRepository.java
+++ backend/src/main/java/com/example/backend/favorites/FavoritesRepository.java
@@ -4,8 +4,11 @@ import com.example.backend.hotel.entity.Favorites;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FavoritesRepository extends JpaRepository<Favorites, Long> {
     boolean existsByUser_IdAndHotel_Id(Long userId, Long hotelId);
+    List<Favorites> findAllByUser_Id(Long userId);
 }
 
