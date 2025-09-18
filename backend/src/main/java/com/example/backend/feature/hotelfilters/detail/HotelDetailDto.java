cd2a856c0db362c3c8685796e427656933057418
JHS5020 <goals00002@naver.com>
2025-09-18 21:50:03 +0900

    확실하진 않음 얼추 끝남
    근데 정확히 무엇인지를...

--- backend/src/main/java/com/example/backend/feature/hotelfilters/detail/HotelDetailDto.java
+++ backend/src/main/java/com/example/backend/feature/hotelfilters/detail/HotelDetailDto.java
@@ -1,12 +1,12 @@
-package com.example.backend.feature.hotelfilters.detail;
-
-import com.example.backend.feature.hotelfilters.dto.HotelDto;
-
-import java.util.List;
-
-public class HotelDetailDto extends HotelDto {
-    private List<String> amenities;
-    private List<Room> rooms;
-    private String description;
-    private List<ReviewDto> reviews;
-}
+//package com.example.backend.feature.hotelfilters.detail;
+//
+//import com.example.backend.feature.hotelfilters.dto.HotelDto;
+//
+//import java.util.List;
+//
+//public class HotelDetailDto extends HotelDto {
+//    private List<String> amenities;
+//    private List<Room> rooms;
+//    private String description;
+//    private List<ReviewDto> reviews;
+//}
